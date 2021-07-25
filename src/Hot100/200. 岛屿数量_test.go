646d485cec4f62e62caeb4f925874b79728de6db
yeyuc <yeyucheng_uestc@163.com>
2021-07-26 07:20:31 +0800

    2021.07.26：#Xiaoxin# Hot100 200. 岛屿数量

---
+++ src/Hot100/200. 岛屿数量_test.go
@@ -0,0 +1,33 @@
+package Hot100
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type RobInput struct {
+	nums []int
+}
+
+type RobOutput struct {
+	ret int
+}
+
+func TestRob(t *testing.T) {
+
+	inputs := []RobInput{
+		{[]int{1, 2, 3, 1}},
+		{[]int{2, 7, 9, 3, 1}},
+	}
+
+	expected := []RobOutput{
+		{4},
+		{12},
+	}
+
+	for i := 0; i < len(inputs); i++ {
+		input := inputs[i]
+		ret := numIslands(input.nums)
+		assert.Equal(t, expected[i].ret, ret)
+	}
+}
